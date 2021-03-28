f0761fdfaeb38d62d3f067387e3d6ee99aab7aa4
Viknesh Mohan <vimohan@microsoft.com>
2021-03-28 10:31:03 +0530

    Hackathon

--- Chapter2/TodoListAPI/Startup.cs
+++ Chapter2/TodoListAPI/Startup.cs
@@ -103,6 +103,7 @@ namespace TodoListAPI
             }
             else
             {
+                Console.WriteLine("in prod" + Configuration["AppClientId"]);
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
